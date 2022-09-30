d3a01c3dfdfd91c658becaf6baa2da6ec6135424
Bharath-SR <bharath.s0941@gmail.com>
2022-09-30 07:25:09 +0530

    UC1: Name Validator is added

--- RegularExpression/src/com/bridgeLabz/RegularExpression.java
+++ RegularExpression/src/com/bridgeLabz/RegularExpression.java
@@ -3,6 +3,9 @@
  */
 package com.bridgeLabz;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author Admin
  *
@@ -10,11 +13,30 @@ package com.bridgeLabz;
 public class RegularExpression {
 
 	/**
-	 * @Starting Regulae Expressions
+	 * @param args
 	 */
+	public static boolean checkValidPincode(String firstName) {
+		String firstNameRegex= "^[A-Z]{1}[A-Za-z]{2,}$";
+		Pattern pattrenObject= Pattern.compile(firstNameRegex);
+		
+		if(firstName==null) {
+			return false;
+			
+		}
+		Matcher matcherObject = pattrenObject.matcher(firstName);
+        return matcherObject.matches();
+	} 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("----- Name Validator -----");
+		
+		String firstName ="Bha";
+		boolean isfirstName = checkValidPincode(firstName);
+		
+		if(isfirstName)
+			System.out.println(firstName+" is an Valid firstName");
+		else
+			System.out.println(firstName+" is an Invalid firstName");
 	}
 
 }
